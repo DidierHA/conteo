2e038346a3a6a601e7a41af0b057f8d7b76f78ad
Didier <didier31cr@hotmail.com>
2021-05-11 21:33:31 -0600

    Subiendo otro proyecto

---
+++ app.js
@@ -0,0 +1,18 @@
+const contador = () => {
+
+    const fechaDeseada = new Date('January 1, 2022 00:00:00').getTime();
+    const fechaHoy = new Date().getTime();
+    const diferencia = fechaDeseada - fechaHoy;
+
+    const segundos = 1000;
+    const minutos = segundos * 60;
+    const horas = minutos * 60;
+    const dias = horas * 24;
+
+    document.querySelector('#dias').innerText = Math.floor(diferencia / dias);
+    document.querySelector('#horas').innerText = Math.floor((diferencia % dias) / horas);
+    document.querySelector('#minutos').innerText = Math.floor((diferencia % horas) / minutos);
+    document.querySelector('#segundos').innerText = Math.floor((diferencia % minutos) / segundos);
+}
+
+setInterval(contador, 1000);
